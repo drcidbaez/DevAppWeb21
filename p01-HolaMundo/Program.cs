aaaccb1708620ff202f916450766cbdcd7f57d98
drcidbaez <doc.geek.one@gmail.com>
2021-08-25 23:27:42 -0500

    Ver1.4

---
+++ p01-HolaMundo/Program.cs
@@ -0,0 +1,24 @@
+﻿// Hola Mundo!
+using System;
+
+namespace p01_HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            DateTime fecha = DateTime.Now;
+            Console.WriteLine("Hola Mundo! \n");
+            Console.WriteLine("Nombre ?");
+            nombre = Console.ReadLine();
+            Console.WriteLine($"{nombre} Bienvenido al C#\n");
+            Console.WriteLine($"Hoy es {fecha:d} y son las {fecha:t}");
+            Console.WriteLine("\nHasta pronto");
+            
+
+
+
+        }
+    }
+}
---
+++ p02-areaCirculo/Program.cs
@@ -0,0 +1,22 @@
+﻿// Calcular el área de un circulo
+
+using System;
+
+namespace p02_areaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float radio=0;
+            double area=0;
+            Console.WriteLine("Área de un Círculo");
+            Console.WriteLine("Proporciona el Radio (r)= ");
+            radio = float.Parse(Console.ReadLine() );
+            area = Math.PI * Math.Pow(radio,2);
+            Console.WriteLine($"El área del círculo es {area}");
+
+
+        }
+    }
+}
