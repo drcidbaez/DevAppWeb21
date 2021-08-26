1156f07e1a92008593d1f004f527126bc08d62d9
drcidbaez <doc.geek.one@gmail.com>
2021-08-25 23:34:03 -0500

    Ver1.5

--- p02-areaCirculo/Program.cs
+++ p02-areaCirculo/Program.cs
@@ -8,13 +8,17 @@ namespace p02_areaCirculo
     {
         static void Main(string[] args)
         {
-            float radio=0;
+            float radio=0; //Definir variable
             double area=0;
             Console.WriteLine("Área de un Círculo");
             Console.WriteLine("Proporciona el Radio (r)= ");
             radio = float.Parse(Console.ReadLine() );
-            area = Math.PI * Math.Pow(radio,2);
+            area = Math.PI * Math.Pow(radio,2); //Elevar al cuadrado
             Console.WriteLine($"El área del círculo es {area}");
+
+            /* radio = float.Parse(Console.ReadLine() );
+            area = Math.PI * Math.Pow(radio,2); //Elevar al cuadrado
+            Console.WriteLine($"El área del círculo es {area}"); */
 
 
         }
